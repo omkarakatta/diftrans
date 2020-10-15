2377d4bced9024256459e2fe525fe3987b82b34d
Omkar A. Katta <omkarakatta@gmail.com>
2020-10-15 11:58:05 -0400

    add option to include user-specified cost matrix
    - if costmat or costm_main or costm_control are NULL, then proceed as
      before. Otherwise, the specified cost matrix will be used.
    - note that a few changes were made to the support in get_OTcost: the
      names were removed, but this is largely unecessary. It is useful for
      interactive verification.
    - documentation was also updated.

--- R/computingOT.R
+++ R/computingOT.R
@@ -37,12 +37,14 @@ build_costmatrix2 <- function(support_pre, support_post, bandwidth = 0){
 ### Compute Transport Cost ---------------------------
 
 #' @importFrom transport transport
-get_OTcost <- function(pre_df, post_df, support = NULL, bandwidth = 0, var = MSRP){
+get_OTcost <- function(pre_df, post_df, support = NULL, bandwidth = 0, var = MSRP, costmat = NULL){
   # given pre-data and post-data, compute optimal transport cost given bandwidth
   pre <- pre_df$count
   post <- post_df$count
   pre_support <- pre_df %>% dplyr::select({{var}}) %>% unlist()
   post_support <- post_df %>% dplyr::select({{var}}) %>% unlist()
+  names(pre_support) <- c()
+  names(post_support) <- c()
 
   if (!identical(pre_support, post_support)){
     stop("`pre_df` and `post_df` need to have the same support")
@@ -50,6 +52,7 @@ get_OTcost <- function(pre_df, post_df, support = NULL, bandwidth = 0, var = MSR
 
   if (is.null(support)){
     support <- pre_df %>% dplyr::select({{var}}) %>% unlist()
+    names(support) <- c()
   }
 
   if (!identical(support, pre_support)){
@@ -59,29 +62,39 @@ get_OTcost <- function(pre_df, post_df, support = NULL, bandwidth = 0, var = MSR
     stop("`support` is different from `pre_support` and `post_support`")
   }
 
-  costm <- build_costmatrix(support, bandwidth)
+  if (is.null(costmat)) {
+    costm <- build_costmatrix(support, bandwidth)
+  }
+
   OT <- transport(
     as.numeric(sum(post) / sum(pre) * pre),
     as.numeric(post),
     costm
   )
 
-  support_pre <- pre_df %>%
-    dplyr::filter(count != 0) %>%
-    dplyr::select({{var}}) %>%
-    dplyr::distinct({{var}}) %>%
-    dplyr::arrange({{var}}) %>%
-    dplyr::filter(!is.na({{var}})) %>%
-    unlist()
+  if (is.null(costmat)) {
+    # construct minimal support and determine cost
+    support_pre <- pre_df %>%
+      dplyr::filter(count != 0) %>%
+      dplyr::select({{var}}) %>%
+      dplyr::distinct({{var}}) %>%
+      dplyr::arrange({{var}}) %>%
+      dplyr::filter(!is.na({{var}})) %>%
+      unlist()
 
-  support_post <- post_df %>%
-    dplyr::filter(count != 0) %>%
-    dplyr::select({{var}}) %>%
-    dplyr::distinct({{var}}) %>%
-    dplyr::arrange({{var}}) %>%
-    dplyr::filter(!is.na({{var}})) %>%
-    unlist()
-  costm_ref <- build_costmatrix2(support_pre, support_post, bandwidth)
+    support_post <- post_df %>%
+      dplyr::filter(count != 0) %>%
+      dplyr::select({{var}}) %>%
+      dplyr::distinct({{var}}) %>%
+      dplyr::arrange({{var}}) %>%
+      dplyr::filter(!is.na({{var}})) %>%
+      unlist()
+
+    costm_ref <- build_costmatrix2(support_pre, support_post, bandwidth)
+
+  } else {
+    costm_ref <- costm
+  }
 
   temp <- as.data.frame(OT) %>%
     dplyr::rowwise() %>%
@@ -112,7 +125,9 @@ get_OTcost <- function(pre_df, post_df, support = NULL, bandwidth = 0, var = MSR
 #'   mass of each value in the support.
 #' }
 #' Since column 1 contains the full support of \code{var} and all these distributions
-#' are of \code{var}, column 1 must be the same for all distributions
+#' are of \code{var}, column 1 must be the same for all distributions.
+#'
+#' The cost matrices specified by \code{costm} should use the minimal support of the distributions.
 #'
 #' @param pre_main probability mass function (see "Details") for \code{var} of the
 #'     treated group before treatment occurs
@@ -139,6 +154,10 @@ get_OTcost <- function(pre_df, post_df, support = NULL, bandwidth = 0, var = MSR
 #' @param suppress_progress_bar if \code{TRUE}, the progress bar will be suppressed; default is \code{FALSE}
 #' @param save_dit if \code{TRUE}, the differences-in-transports estimator as
 #'     well as the associated bandwidth will be returned
+#' @param costm_main if \code{NULL}, the cost matrix will be such that if the transport 
+#'     distance is greater than what is specified in \code{bandwidth_seq}, cost is 1 and 0 otherwise.
+#' @param costm_control if \code{NULL}, the cost matrix will be such that if the transport 
+#'     distance is greater than what is specified in \code{bandwidth_seq}, cost is 1 and 0 otherwise.
 #'
 #' @return a data.frame with the transport costs associated with each value of \code{bandwidth_seq}.
 #' \itemize{
@@ -161,7 +180,6 @@ get_OTcost <- function(pre_df, post_df, support = NULL, bandwidth = 0, var = MSR
 #'
 #' @export
 #' @importFrom rlang enquo
-#'
 #' @examples
 #' # Find conservative transport cost of MSRP in Beijing between 2010 and 2011 using bandwidth = 0
 #' # # step 1: find support
@@ -241,7 +259,9 @@ diftrans <- function(pre_main = NULL, post_main = NULL,
                      conservative = F,
                      quietly = F,
                      suppress_progress_bar = F,
-                     save_dit = F){
+                     save_dit = F,
+                     costm_main = NULL,
+                     costm_control = NULL){
   var <- rlang::enquo(var)
   # error checking
   if (is.null(pre_main) | is.null(post_main)){
@@ -285,9 +305,9 @@ diftrans <- function(pre_main = NULL, post_main = NULL,
     if (!suppress_progress_bar) utils::setTxtProgressBar(pb, i)
 
     bandwidth <- bandwidth_seq[i]
-    main_cost <- get_OTcost(pre_main, post_main, bandwidth = bandwidth, var = !!var)
-    if (conservative) maincons_cost <- get_OTcost(pre_main, post_main, bandwidth = 2*bandwidth, var = !!var)
-    if (est == "dit") control_cost <- get_OTcost(pre_control, post_control, bandwidth = bandwidth, var = !!var)
+    main_cost <- get_OTcost(pre_main, post_main, bandwidth = bandwidth, var = !!var, costmat = costm_main)
+    if (conservative) maincons_cost <- get_OTcost(pre_main, post_main, bandwidth = 2*bandwidth, var = !!var, costmat = costm_main)
+    if (est == "dit") control_cost <- get_OTcost(pre_control, post_control, bandwidth = bandwidth, var = !!var, costmat = costm_control)
 
     main_prop[i] <- main_cost$prop_bribe
     if (conservative) maincons_prop[i] <- maincons_cost$prop_bribe
